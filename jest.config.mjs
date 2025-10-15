b8a3ed1b2591dc83a6756c09fc1db39739c79551
Carla Valdivia <valdivia.carla@gmail.com>
2025-10-15 18:49:19 -0300

    install: libs e dependencias
    Instalacao de dependencias do projeto como styled components, babel plugin styled components, jest, jest styled components, json-server

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
+  
+  testEnvironment: 'jest-environment-jsdom',
+  
+}
+
+export default createJestConfig(customJestConfig);
---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+// Import custom matchers (like .toBeInTheDocument())
+import '@testing-library/jest-dom'
+
+// Imports styled-components' serializer.
+import 'jest-styled-components'
