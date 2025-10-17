797dea003dec874de6901b9c5d1fca484098b5bd
Carla Valdivia <valdivia.carla@gmail.com>
2025-10-17 16:43:48 -0300

    feat: Esqueleto do Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+'use client'
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  max-width: 1440px;
+`
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+
+    </StyledFooter>
+  )
+}
+
+export { StyledFooter };
+export default Footer;
