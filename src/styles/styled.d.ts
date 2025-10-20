c4dfde95853654bb9dcdb7f01223299f17375ad3
Carla Valdivia <valdivia.carla@gmail.com>
2025-10-20 18:56:21 -0300

    fix: adiciona regra para consertar eslint

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,9 +1,11 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
+
 import 'styled-components'
 import { theme } from './theme'
 
 type ThemeType = typeof theme
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeType {
+    _?: never;
+  }
 }
