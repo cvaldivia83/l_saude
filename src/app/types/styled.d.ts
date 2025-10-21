b1ac772e115c5622301936c3d42e784e8674859a
Carla Valdivia <valdivia.carla@gmail.com>
2025-10-21 09:16:09 -0300

    fix: remocao do antigo types e implementacao de um novo para sanar erro de build

---
+++ src/app/types/styled.d.ts
@@ -0,0 +1,13 @@
+
+import 'styled-components'; 
+
+import type { Theme } from '@/styles/theme';
+
+
+declare module 'styled-components' {
+
+  interface DefaultTheme extends Theme {
+    __typeGuard?: unknown;
+  }
+ 
+}
--- styled.d.ts
+++
@@ -1,7 +0,0 @@
-import 'styled-components';
-import { Theme } from './src/styles/theme';
-
-declare module 'styled-components' {
-  // Sobrescreve a DefaultTheme com a sua interface Theme
-  export interface DefaultTheme extends Theme {}
-}
